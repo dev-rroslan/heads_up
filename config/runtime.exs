f91336973a9ab86b072ed9713f10698a4a2a5477
dev-rroslan <dev.rroslan@gmail.com>
2025-03-28 12:35:35 +0800

    edit runtime http://localhost

--- config/runtime.exs
+++ config/runtime.exs
@@ -63,7 +63,7 @@ if config_env() == :prod do
 
   config :heads_up, HeadsUpWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
-    check_origin: ["https://applikasi.tech", "https://www.applikasi.tech", "localhost"],
+    check_origin: ["https://applikasi.tech", "https://www.applikasi.tech", "http://localhost"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
